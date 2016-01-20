91c7a3ed9dbced8ed675796d22db530cbe0a0f2b
Gambit <chernozor@yandex.ru>
2016-01-20 12:29:40 +0300

    release

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //2. Убрать вкладку Описание
 
-function formCheck(){
+function formCheck() {
 
-    var errMsg = {'both':'Заполните обязательные поля'};
+    var errMsg = {'both': 'Заполните обязательные поля'};
 
     var formError = '';
     if ((jQuery('#jform_params_app_key').val() == '') || (jQuery('#jform_params_email').val() == '')) {
@@ -17,13 +17,13 @@ function formCheck(){
 
 }
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     jQuery('a[href="#description"]').hide();
 
     //кнопка проверить ключ
-    var text_after = "Введите email и ключ API из личного кабинета inTarget. <br>"+
+    var text_after = "Введите email и ключ API из личного кабинета inTarget. <br>" +
         "Если вы еще не регистрировались в сервисе inTarget это можно сделать по ссылке <a href='http://intarget.ru'>inTarget.ru</a>";
-    var support_text =   "<br><br>Служба поддержки: <a href='mailto:plugins@intarget.ru'>plugins@intarget.ru</a><br>Joomla inTarget v1.0.0";
+    var support_text = "<br><br>Служба поддержки: <a href='mailto:plugins@intarget.ru'>plugins@intarget.ru</a><br>Joomla inTarget v1.0.0";
     var success_text = "Поздравляем! Ваш сайт успешно привязан к аккаунту <a href='http://intarget.ru'>intarget.ru</a>. Войдите в личный кабинет для просмотра статистики."
 
     ////проверка на успешную регистрацию.
@@ -34,19 +34,18 @@ jQuery(document).ready(function(){
 
     if ((jQuery('#jform_params_app_key').val() !== '') && (jQuery('#jform_params_email').val() !== '')) {
         if (window.intarget_succes_reg == true) {
-            console.log("1");
             jQuery('#jform_params_app_key').after('<img title="Введен правильный ключ!" class="intrg_ok" src="../plugins/system/intarget/ok.png">');
             jQuery('#jform_params_email').after('<img title="Введен правильный email!" class="intrg_ok" src="../plugins/system/intarget/ok.png">');
             jQuery('#jform_params_intarget_id').val(window.intarget_id);
-           //выключаем поля
-           // jQuery('#jform_params_app_key').attr('disabled', 'disabled');
-           // jQuery('#jform_params_email').attr('disabled', 'disabled');
-            jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(success_text+support_text);
+            //выключаем поля
+            // jQuery('#jform_params_app_key').attr('disabled', 'disabled');
+            // jQuery('#jform_params_email').attr('disabled', 'disabled');
+            jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(success_text + support_text);
             //jQuery('button#intrgt_auth_btn').hide();
         } else if (window.intarget_succes_reg == false) {
-            jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(text_after+support_text);
+            jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(text_after + support_text);
             console.log("2");
-            if (window.intarget_reg_error == 403){
+            if (window.intarget_reg_error == 403) {
                 jQuery('.alert.alert-success').hide();
                 var error_text = 'Неверно введен email или ключ API.';
                 //jQuery('#intrgt_auth_btn').after('<div><span class="intrg_err"> <b>Ошибка!</b> Неверно введен email или ключ API.</span></div>')
@@ -62,52 +61,38 @@ jQuery(document).ready(function(){
                 var error_text = 'Данный email не зарегистрирован на сайте <a href="http://intarget.ru">http://intarget.ru</a>';
                 //jQuery('#intrgt_auth_btn').after('<span class="intrg_err"> <b>Ошибка!</b> Данный email не зарегистрирован на сайте <a href="http://intarget.ru">http://intarget.ru</a></span>')
             }
-            var intrg_btn_html = '<div style="width:100%;margin-top: 20px;">'+
-                '<button style="float:left;" id="intrgt_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>'+
-                '<div style="padding-top: 2px;">'+
-                '<span class="intrg_err" style="margin-left: 5px;"> <b>Ошибка!</b> '+ error_text +
-                '</span>'+
-                '</div>'+
+            var intrg_btn_html = '<div style="width:100%;margin-top: 20px;">' +
+                '<button style="float:left;" id="intrgt_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>' +
+                '<div style="padding-top: 2px;">' +
+                '<span class="intrg_err" style="margin-left: 5px;"> <b>Ошибка!</b> ' + error_text +
+                '</span>' +
+                '</div>' +
                 '</div>';
             jQuery('input#jform_params_app_key').parent().after(intrg_btn_html)
         } else {
-            console.log('3');
-            jQuery('input#jform_params_app_key').parent().after('<button style="float:left;margin-top: 15px;" id="intrgt_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>')
-            jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(text_after+support_text);};
-
-
+            if (jQuery('#jform_enabled').val() == 0) {
+                var error_text = 'Для начала работы необходимо включить плагин.';
+            }
+            jQuery('input#jform_params_app_key').parent().after('<button style="float:left;margin-top: 15px;" id="intrgt_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>' +
+                '<div style="padding-top: 20px;">' +
+                '<span class="intrg_err" style="margin-left: 5px;"> <b>Ошибка!</b> ' + error_text +
+                '</span>' +
+                '</div>');
+            jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(text_after + support_text);
+        }
     } else {
-        jQuery('input#jform_params_app_key').parent().after('<button style="float:left;margin-top: 15px;" id="intrgt_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>')
-        jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(text_after+support_text);};
+        jQuery('input#jform_params_app_key').parent().after('<button style="float:left;margin-top: 15px;" id="intrgt_auth_btn" onclick="formCheck(); return false;"> Авторизация </button>');
+        jQuery('[id=jform_params_app_key-lbl]').parent().parent().after(text_after + support_text);
+    }
 
     //правим отступ
-    jQuery('#jform_params_app_key').parent().css('margin-left','70px')
-    jQuery('#jform_params_email').parent().css('margin-left','70px')
-
-
-   /* jQuery('#intrgt_auth_btn').attr('disabled', 'disabled');
-    jQuery('#jform_params_app_key, #jform_params_email').change(function() {
-
-        var empty = false;
-        jQuery('#jform_params_app_key, #jform_params_email').each(function() {
-            if (jQuery(this).val() == '') {
-                empty = true;
-            }
-        });
-        if (empty) {
-            jQuery('#intrgt_auth_btn').attr('disabled', 'disabled');
-        } else {
-            jQuery('#intrgt_auth_btn').removeAttr('disabled');
-        }
-    });
-    */
-
-})
-
+    jQuery('#jform_params_app_key').parent().css('margin-left', '70px');
+    jQuery('#jform_params_email').parent().css('margin-left', '70px')
+});
 
 
 //добавляем вместо краткого описания нужный текст.
-var  text_after2 = "<p><b>inTarget</b> - сервис повышения продаж и аналитика посетителей сайта.</p>" +
+var text_after2 = "<p><b>inTarget</b> - сервис повышения продаж и аналитика посетителей сайта.</p>" +
     "<p>Оцените принципиально новый подход к просмотру статистики. Общайтесь со своей аудиторией, продавайте лучше, зарабатывайте больше. И все это бесплатно!</p>";
 jQuery('.readmore').parent().hide();
 jQuery('.info-labels').after(text_after2);
